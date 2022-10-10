3ffcf4ad074f0eba29c047a71dde96a18c7dcf7d
webbalaka <webbalaka@gmail.com>
2022-10-10 21:40:06 +0700

    My First website commit

--- main.js
+++ main.js
@@ -6,17 +6,22 @@ window.addEventListener("scroll", function(){
 window.addEventListener("scroll", function(){ 
     const rocket = document.querySelector(".rocket");
     const rocket_fire = document.querySelector(".rocket-fire");
-    const school1Hide = document.querySelector(".school1-hide");
-    const school2Hide = document.querySelector(".school2-hide");
-    const school3Hide = document.querySelector(".school3-hide");
     rocket.classList.toggle("rocket-lunch", window.scrollY >= 1200)
     rocket_fire.classList.toggle("rocket-fire-lunch", window.scrollY >= 1200)
-    // school1Hide.classList.toggle("school1-show", window.scrollY >= 1500)
-    // school2Hide.classList.toggle("school2-show", window.scrollY >= 1500)
-    // school3Hide.classList.toggle("school3-show", window.scrollY >= 1500)
-
 });
 
+
+const scrollToTOP = document.querySelector(".scrollToTop");
+// const scrollToTopActivate = document.querySelector(".scrollToTopActivate");
+
+// window.addEventListener("scroll", function(){
+//     scrollToTOP.classList.toggle("scrollToTopActivate", window.scrollY>= 500)
+// });
+
+scrollToTOP.addEventListener("click", function(){
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0;
+});
 
 
 
